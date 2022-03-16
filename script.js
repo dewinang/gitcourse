be41a2e375a3650b267d03f4a63e7bf4f93c2ee9
dewinang <zlbober@gmail.com>
2022-03-16 18:39:16 +0200

    16-03-2022 18:39

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.body.innerHTML='<h1>Page Loading...</h1>'
 console.log('Fetching!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!...')
 console.log('something')
 console.log('something')
+console.log('new work')
