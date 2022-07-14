8493fe650773186093efc3a41acd7ff9adfaa421
Dewin <zlbober@gmail.com>
2022-07-14 15:53:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.body.innerHTML='<h1>Page Loading...</h1>'
-console.log('Fetching!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!...')
-console.log('something')
-console.log('something')
-console.log('new work')
-console.log('one more')
+
