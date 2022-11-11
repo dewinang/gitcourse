935d7323515c51b2e24d3750246afd2707ed299f
victor.pavuk <zlbober@gmail.com>
2022-11-11 20:24:32 +0200

    RTYUIO

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         DEWIN
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 3
-        </a>
+       
       </header>
     </div>
   );
